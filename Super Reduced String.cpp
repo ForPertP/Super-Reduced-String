352754085e53075621e2134a6166c308b6b8a1ee
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-08-31 04:23:46 +0900

    Update Super Reduced String.cpp

--- Super Reduced String.cpp
+++ Super Reduced String.cpp
@@ -1,1 +1,20 @@
+string superReducedString(string s)
+{
+    for (int i = 0; i < s.length()-1; i++)
+    {
+        if (s[i] == s[i+1])
+        {
+            s.erase(i, 2);
+            i = -1;
+            
+            if (s.empty())
+            {
+                s = "Empty String";
+                break;
+            }
+        }
+    }
+    
+    return s;
+}
 
