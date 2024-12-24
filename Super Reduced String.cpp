96c77cf41ba10e73b35756da967dfee3444f0535
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-12-25 02:15:53 +0900

    Update Super Reduced String.cpp

--- Super Reduced String.cpp
+++ Super Reduced String.cpp
@@ -1,1 +1,47 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+/*
+ * Complete the 'superReducedString' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string superReducedString(string s)
+{
+    int n = s.length();
+    int i = 0;
+
+    for (int j = 0; j < n; ++j)
+    {
+        if (i == 0 || s[i-1] != s[j])
+        {
+            s[i++] = s[j];
+        }
+        else
+        {
+            --i;
+        }
+    }
+
+    return i == 0 ? "Empty String" : s.substr(0, i);
+}
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = superReducedString(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
