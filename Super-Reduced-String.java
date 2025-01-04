f9de424f09516077bde1cc3b65cce74742442e9b
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-01-04 11:41:19 +0900

    Update Super-Reduced-String.java

--- Super-Reduced-String.java
+++ Super-Reduced-String.java
@@ -11,4 +11,20 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
+
+  public static String superReducedString(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (sb.length() == 0 || sb.charAt(sb.length() - 1) != c) {
+                sb.append(c);
+            } else {
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+
+        return sb.length() == 0 ? "Empty String" : sb.toString();
+    }
 }
+
+
